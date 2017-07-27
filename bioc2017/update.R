a34f908c3c98f906938d6099841851f32d5a10ec
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-07-27 11:12:14 -0400

    rebuilding site Thu Jul 27 11:12:14 EDT 2017

--- bioc2017/update.R
+++ bioc2017/update.R
@@ -1,2 +1,3 @@
+library(httr)
 write(httr::content(GET("https://raw.githubusercontent.com/waldronlab/MicrobiomeWorkshop/master/vignettes/MicrobiomeWorkshop.Rmd")), file = "MicrobiomeWorkshop.Rmd")
 file.edit("MicrobiomeWorkshop.Rmd")
